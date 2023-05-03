8da9fdaece9de80233f00a5ae64ccc97c3586efc
羽墨 <69838566+yumo0@users.noreply.github.com>
2023-05-03 16:24:33 +0800

    eureka server单机配置

---
+++ cloud-eureka-server7001/src/main/java/top/yumoyumo/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package top.yumoyumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: yumo
+ * @Description: TODO
+ * @DateTime: 2023/5/3 15:53
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
